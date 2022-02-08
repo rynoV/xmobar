5c57d149a22116f3ffc6cc4e6aa2430ccd1f0382
jao <jao@gnu.org>
2022-02-08 15:14:41 +0000

    Missing help bit: -TSwaybar

--- src/Xmobar/App/Opts.hs
+++ src/Xmobar/App/Opts.hs
@@ -61,7 +61,7 @@ options =
     , Option "r" ["recompile"] (NoArg Recompile) "Force recompilation"
     , Option "V" ["version"] (NoArg Version) "Show version information"
     , Option "T" ["text"] (OptArg TextOutput "color")
-             "Write text-only output to stdout. Plain/Ansi/Pango"
+             "Write text-only output to stdout. Plain/Ansi/Pango/Swaybar"
     , Option "f" ["font"] (ReqArg Font "font name") "Font name"
     , Option "N" ["add-font"] (ReqArg AddFont "font name")
              "Add to the list of additional fonts"
