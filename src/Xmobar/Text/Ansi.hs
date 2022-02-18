rynoV/xmobar

commit b7ca54651bb12ac7335d525cdd4962c765ec6d0c

"bright" ansi colour names
See #606 for further discussion

src/Xmobar/Text/Ansi.hs
@@ -17,6 +17,7 @@
 module Xmobar.Text.Ansi (withAnsiColor) where
 
 import Data.List (intercalate)
+import Data.Char (toLower)
 
 asInt :: String -> String
 asInt x = case (reads $ "0x" ++ x)  :: [(Integer, String)] of
@@ -25,9 +26,13 @@ asInt x = case (reads $ "0x" ++ x)  :: [(Integer, String)] of
 
 namedColor :: String -> String
 namedColor c =
-  case c of
+  case (map toLower c) of
     "black" -> "0"; "red" -> "1"; "green" -> "2"; "yellow" -> "3"; "blue" -> "4";
-    "magenta" -> "5"; "cyan" -> "6"; "white" -> "7"; _ -> ""
+    "magenta" -> "5"; "cyan" -> "6"; "white" -> "7";
+    "brightblack" -> "8"; "brightred" -> "9"; "brightgreen" -> "10";
+    "brightyellow" -> "11"; "brightblue" -> "12";
+    "brightmagenta" -> "13"; "brightcyan" -> "14"; "brightwhite" -> "15";
+    _ -> ""
 
 ansiCode :: String -> String
 ansiCode ('#':r:g:[b]) = ansiCode ['#', '0', r, '0', g, '0', b]

commit 5492821f70dc5f3646a239fe66d7cb9d5ca76bf7

Apply hlint suggestion
Fixes: b7ca54651bb1 (""bright" ansi colour names")

src/Xmobar/Text/Ansi.hs
@@ -26,7 +26,7 @@ asInt x = case (reads $ "0x" ++ x)  :: [(Integer, String)] of
 
 namedColor :: String -> String
 namedColor c =
-  case (map toLower c) of
+  case map toLower c of
     "black" -> "0"; "red" -> "1"; "green" -> "2"; "yellow" -> "3"; "blue" -> "4";
     "magenta" -> "5"; "cyan" -> "6"; "white" -> "7";
     "brightblack" -> "8"; "brightred" -> "9"; "brightgreen" -> "10";